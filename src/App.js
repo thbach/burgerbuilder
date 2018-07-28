5933ba792b765bf158d8b988e76ff4b9604242b5
tbach <thbach@gmail.com>
2018-07-28 01:42:08 -0700

    added burger controls

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
